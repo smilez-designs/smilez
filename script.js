f7a969ed279d8fd8ff14d7b0269047840127cfdf
smilez-designs <mileslennon1@gmail.com>
2026-04-10 04:08:49 +0000

    Initial Smilez portfolio setup

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Smilez portfolio loaded");
+const buttons = document.querySelectorAll(".filter-btn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+  });
+});
